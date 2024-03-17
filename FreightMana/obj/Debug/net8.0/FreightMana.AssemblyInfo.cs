d2b18d6af4ae98724fa3b631229fb363b369f5ab
nguyetnhi103 <nguyetnhi10032003@gmail.com>
2024-03-17 18:11:33 +0700

    update

--- FreightMana/obj/Debug/net8.0/FreightMana.AssemblyInfo.cs
+++ FreightMana/obj/Debug/net8.0/FreightMana.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreightMana")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03a28e3b9c9bb4af4fe61c561eb45f00d1d58418")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5082044c2ab69ff859eefc933a508d704a1e8565")]
->>>>>>> d90043d87b3f9eb242ee06d6f1551a76cbcfc0f4
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreightMana")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreightMana")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
